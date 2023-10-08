f4fccffc2cff74b2c929711f249b7812adb57b71
lighuennqn <lighuenblanca@gmail.com>
2023-10-08 16:34:04 -0300

    aplicación de funciones

--- index.js
+++ index.js
@@ -1,9 +1,34 @@
+/* Variables */
 let balance = 0;
 let opcion = 0;
-let semanas = 0; 
+let semanas = 0;
 let porcentaje = 21;
 let haberes = 0;
-    
+
+/* Funciones*/
+
+function ivaFiltro(periodos) {
+    for (let i = 1; i <= periodos; i++) {
+        let numeroIngresado = parseInt(prompt("Ingrese sus ingresos de la semana " + i));
+        let valorConPorcentaje = (porcentaje / 100) * numeroIngresado;
+        balance = balance + (numeroIngresado - valorConPorcentaje);
+    }
+}
+function saldoNeto(saldo) {
+    if (saldo > 0) {
+        alert("Esta semana has tenido ganancias.")
+    }
+
+    else if (saldo < 0) {
+        alert("Esta semana has tenido pérdidas.")
+    }
+
+    else {
+        alert("Esta semana no has perdido ni ganado dinero.")
+    }
+}
+
+/* MAIN */
 alert("Bienvenidos al sistema de cálculos de ganacias mensuales, ejecutado semana por semana.");
 
 while (opcion != 1 && opcion != 2) {
@@ -11,43 +36,34 @@ while (opcion != 1 && opcion != 2) {
     Opción 1) 4 semanas. 
     Opción 2) 5 semanas.`))
 
-       if (opcion == 1){
-       semanas = 4
-       console.log("hola entre a op1")
-       }
-    else if (opcion == 2){
-      semanas = 5 
-      console.log("hola entre a op2")    
+    if (opcion == 1) {
+        semanas = 4
+        console.log("hola entre a op1")
+    }
+    else if (opcion == 2) {
+        semanas = 5
+        console.log("hola entre a op2")
     }
     else {
         alert("Ingrese una opción válida")
     }
-    }
+}
 
-    for (let i = 1; i <= semanas; i++){
-        let numeroIngresado = parseInt (prompt("Ingrese sus ingresos de la semana " + i));
-        let valorConPorcentaje = (porcentaje / 100) * numeroIngresado;
-        balance = balance + (numeroIngresado - valorConPorcentaje);
-    }
-    balance =  Math.trunc(balance) 
+ivaFiltro(semanas)
 
-    alert ("El balance sin el 21% del IVA es de: $" + balance)
+balance = Math.trunc(balance)
 
-    haberes = parseInt (prompt("Ingrese los haberes(gastos del mes)"));
+alert("El balance sin el 21% del IVA es de: $" + balance)
 
-    let balanceFinal = balance - haberes
+haberes = parseInt(prompt("Ingrese los haberes(gastos del mes)"));
 
-    alert ("Y el balance final restados los gastos es de $" + balanceFinal)
 
-    if (balanceFinal > 0){
-    alert ("Esta semana has tenido ganancias.")
-        }
+let balanceFinal = balance - haberes
 
-    else if (balanceFinal <0){
-        alert ("Esta semana has tenido pérdidas.")
-    }
+alert("Y el balance final restados los gastos es de $" + balanceFinal)
 
-    else {
-        alert ("Esta semana no has perdido ni ganado dinero.")
-    }
+
+saldoNeto(balanceFinal)
+
+
 
